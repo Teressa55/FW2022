f7e57d59ab39cffcdc0c7958e9400927276283e0
Teressa55 <tereza.entlerova@gmail.com>
2022-07-26 22:03:56 +0200

    update_new3pages_26/7_2203

--- registration.js
+++ registration.js
@@ -1,6 +1,4 @@
-
-
-
+//registration
 function addFields(){
   let ol = document.querySelector("ol")
   let number = document.getElementById ("ticketNumber").value
@@ -13,7 +11,7 @@ function addFields(){
         ol.insertBefore(newLi, fileUpload)
 
         let newLabel = document.createElement("label")
-        newLabel.innerHTML = "Other FW Passport: "
+        newLabel.innerHTML = "Other ticket number: "
         newLabel.setAttribute('for', 'ticketNumber ticketNumber--'+ i)
         newLi.appendChild(newLabel)
 
@@ -36,9 +34,101 @@ function addFields(){
   }
 }
 
-// function invalidInput(){
-//     document.getElementById("warning").style.display = "inline-block"
-//     document.querySelector('.validInput').style.border = "2px solid rgb(220, 42, 42)"
-// }
+// myProfile
+function addFW(){
+  // let hiddenFW = document.querySelector("#hiddenFW");
+  // hiddenFW.classList.remove("noFW")
+  // hiddenFW.classList.add("seeFW")
+
+  let ol = document.querySelector("ol")
+  let newLi = document.createElement("li")
+
+  ol.appendChild(newLi)
+  ol.insertBefore(newLi, fileUpload)
+  newLi.id = "hiddenFW"
+  newLi.className = "seeFW"
+
+  let newLabel = document.createElement("label")
+  newLabel.innerHTML = "Add ticket number: "
+  newLabel.setAttribute('for', 'ticketNumber ticketNumber')
+  newLi.appendChild(newLabel)
+
+  var input = document.createElement("input")
+  input.type = "text"
+  input.name = "ticketNumber"
+  input.id = "ticketNumber"
+  input.placeholder = "123XY"
+  input.className = "validInput"
+  input.minLength = "5"
+  input.maxLength = "5"
+  newLi.appendChild(input)
+    }
+
+function removeFW(){
+  let hiddenFW = document.querySelector("#hiddenFW")
+  let ol = document.querySelector("ol")
+
+  ol.removeChild(hiddenFW)
+
+    }
+
+function addPassword(){
+  let hiddenPassword = document.querySelector("#hiddenPassword");
+  hiddenPassword.classList.toggle("noPassword")
+  hiddenPassword.classList.toggle("seePassword")
+}   
+
+function addPubcrawl(){
+  let hiddenPubcrawl = document.querySelector("#hiddenPubcrawl");
+  hiddenPubcrawl.classList.toggle("noPubcrawl")
+  hiddenPubcrawl.classList.toggle("seePubcrawl")
+}
+
+function addPaddles(){
+  let hiddenPaddles = document.querySelector("#hiddenPaddles");
+  hiddenPaddles.classList.toggle("noPaddles")
+  hiddenPaddles.classList.toggle("seePaddles")
+}
+
+function checkPassword() {
+  let password1 = document.getElementById("newPassword").value
+  let password2 = document.getElementById("newPassword_no2").value
+  let passwordMatchResult = document.querySelector(".passwordMatchResult")
+  
+    if (password2.startsWith(password1) && password2 == password1) {
+      passwordMatchResult.innerHTML = "They are the same."
+      return false;
+    }
+    else{
+      passwordMatchResult.innerHTML = "They are not the same."
+      return true;
+    }
+  
+}
+
+function upload(){
+  let uploadButton = document.querySelector("#uploadInfo")
+  let uploadSpan = document.querySelector("#uploadInfo_span")
+  uploadButton.className = "noInfo"
+  uploadSpan.className = "seeInfo_span"
+  if(passwordMatchResult.innerHTML != "They are not the same."){
+  }
+}
 
 
+//forgottenPassword
+function checkPassword2a() {
+  let password1 = document.getElementById("newPassword1a").value
+  let password2 = document.getElementById("newPassword_no2a").value
+  let passwordMatchResult = document.querySelector(".passwordMatchResult2a")
+  
+    if (password2.startsWith(password1) && password2 == password1) {
+      passwordMatchResult.innerHTML = "They are the same."
+      return false;
+    }
+    else{
+      passwordMatchResult.innerHTML = "They are not the same."
+      return true;
+    }
+  
+}
